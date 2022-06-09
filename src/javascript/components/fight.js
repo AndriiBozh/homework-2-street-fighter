e20dc5b7ccc46428fc6e4961b10166b0bbdbba7d
AndriiBozh <deux_jours_avant@hotmail.com>
2022-06-09 12:45:00 +0300

    updated getDamage, getHitPower and getBlockPower functions

--- src/javascript/components/fight.js
+++ src/javascript/components/fight.js
@@ -8,12 +8,24 @@ export async function fight(firstFighter, secondFighter) {
 
 export function getDamage(attacker, defender) {
   // return damage
+  const hitPower = getHitPower(attacker);
+  const blockPower = getBlockPower(defender);
+  const damage = blockPower > hitPower ? 0 : hitPower - blockPower;
+  return damage;
 }
 
 export function getHitPower(fighter) {
   // return hit power
+  const attack = fighter.attack;
+  const criticalHitChance = Math.random() + 1; //random number (not rounded), between 1 and 2
+  const hitPower = attack * criticalHitChance;
+  return hitPower;
 }
 
 export function getBlockPower(fighter) {
   // return block power
+  const defense = fighter.defense;
+  const dodgeChance = Math.random() + 1;
+  const blockPower = defense * dodgeChance;
+  return blockPower;
 }
